1a45e92ef37547f3a785767071a10fdd92e09d22
ganesh <pganeshkumar.2010@gmail.com>
2023-05-15 12:51:05 +0530

    Arithmatic operator program

---
+++ src/arithmaticoperatorslearning/Arithmaticoperators.java
@@ -0,0 +1,35 @@
+package arithmaticoperatorslearning;
+
+public class Arithmaticoperators {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * Arithmaticoperators
+		 * +
+		 * -
+		 * *
+		 * /
+		 * %
+		 */
+		int a=50, b=6;
+		int c;
+		c=a+b;
+		System.out.println("Value of Additional operator " + c);
+		int d;
+		d=a-b;
+		System.out.println("Value of Subtration operator " + d);
+		int e;
+		e=a*b;
+		System.out.println("Value of Multiplication operator " + e);
+		int f;
+		f=a/b;
+		System.out.println("Value of Division operator " + f);
+		int g;
+		g=a%b;
+		System.out.println("Value of Modulus operator " + g);
+		System.out.println("Arithmatic operator program completed");
+	}
+
+}
